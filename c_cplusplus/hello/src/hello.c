70cf01246ae4fc4ccaed56d3bdc827abc278f8c3
kabir-zhong <15618537095@126.com>
2015-12-17 12:36:34 +0800

    add c code

---
+++ c_cplusplus/hello/src/hello.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hello.c
+ Author      : kabir
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello, Eclipse"); /* prints Hello, Eclipse */
+	return EXIT_SUCCESS;
+}
