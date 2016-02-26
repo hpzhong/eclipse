1b77d61d58410ae74b1c5516fd27ea8c3e77c451
kabir <jxwazxzhp@163.com>
2016-02-26 17:07:59 +0800

    add String

--- jni/01/dynamic/Sample.java
+++ jni/01/dynamic/Sample.java
@@ -3,7 +3,9 @@ public class Sample {
 		System.loadLibrary("hello");
 	}
 	static native int hello(int val);
+	static native String prints(String str);
 	public static void main(String [] args) {
 		hello(100);
+		System.out.println(prints("from java"));
 	}
 }
--- jni/01/dynamic/native.c
+++ jni/01/dynamic/native.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
 #include <jni.h>
 
-
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 jint print_hello(JNIEnv *env, jobject cls, jint val)
 {
 	printf("Hello, World!, value=%d\n", val);
 }
+jstring JNICALL print_string(JNIEnv *env, jobject cls, jstring str)
+{
+	const jbyte *cstr;
 
+	cstr = (*env)->GetStringUTFChars(env, str, NULL);
+	if (cstr == NULL) {
+		return NULL; /* Out Of Memory Error already thrown */
+	}
+
+	printf("Get string from java :%s\n", cstr);
+	(*env)->ReleaseStringUTFChars(env, str, cstr);
+
+	return (*env)->NewStringUTF(env, "return from c");
+}
 static const JNINativeMethod methods[] =
 {
 	{"hello", "(I)I", print_hello},
+	{"prints", "(Ljava/lang/String;)Ljava/lang/String;", print_string},
 };
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
@@ -26,7 +40,7 @@ JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
 	}
 
 	/* 2. map java hello <-->c c_hello */
-	if ((*env)->RegisterNatives(env, cls, methods, 1) < 0)
+	if ((*env)->RegisterNatives(env, cls, methods, ARRAY_SIZE(methods)) < 0)
 		return JNI_ERR;
 
 	return JNI_VERSION_1_4;
