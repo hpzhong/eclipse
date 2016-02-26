f673cd4dbe393395235524e11f5f15eaeffee9ca
kabir <jxwazxzhp@163.com>
2016-02-26 17:12:42 +0800

    array

--- jni/01/dynamic/Sample.java
+++ jni/01/dynamic/Sample.java
@@ -3,9 +3,12 @@ public class Sample {
 		System.loadLibrary("hello");
 	}
 	static native int hello(int val);
+	static native int print_array(int[] arr);
 	static native String prints(String str);
 	public static void main(String [] args) {
+		int[] array={1,2,3,4,5};
 		hello(100);
 		System.out.println(prints("from java"));
+		System.out.println(print_array(array));
 	}
 }
--- jni/01/dynamic/native.c
+++ jni/01/dynamic/native.c
@@ -2,10 +2,12 @@
 #include <jni.h>
 
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+
 jint print_hello(JNIEnv *env, jobject cls, jint val)
 {
 	printf("Hello, World!, value=%d\n", val);
 }
+
 jstring JNICALL print_string(JNIEnv *env, jobject cls, jstring str)
 {
 	const jbyte *cstr;
@@ -20,10 +22,27 @@ jstring JNICALL print_string(JNIEnv *env, jobject cls, jstring str)
 
 	return (*env)->NewStringUTF(env, "return from c");
 }
+
+jint print_array(JNIEnv *env, jobject cls, jintArray arr)
+{
+	jint *carr;
+	jint i, sum = 0;
+	carr = (*env)->GetIntArrayElements(env, arr, NULL);
+	if (carr == NULL) {
+		return 0; /* exception occurred */
+	}
+	for (i=0; i< (*env)->GetArrayLength(env, arr); i++) {
+		sum += carr[i];
+	}
+	(*env)->ReleaseIntArrayElements(env, arr, carr, 0);
+	return sum;
+}
+
 static const JNINativeMethod methods[] =
 {
 	{"hello", "(I)I", print_hello},
 	{"prints", "(Ljava/lang/String;)Ljava/lang/String;", print_string},
+	{"print_array", "([I)I", print_array},
 };
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *jvm, void *reserved)
